956916ca4b7d59e12bb74f53c9b6fffdb0616747
Kamil Cholewa <atevin187@gmail.com>
2020-05-29 13:46:21 +0200

    Change file structure

---
+++ core/main.js
@@ -0,0 +1,18 @@
+require.config({
+  paths: {
+    babel: "babel",
+
+    "babel-standalone":
+      "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min",
+    "babel-polyfill":
+      "https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min",
+    vue: "https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.11/vue.min",
+    "http-vue-loader":
+      "https://cdn.jsdelivr.net/npm/http-vue-loader@1.4.2/src/httpVueLoader.min",
+    axios: "https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min",
+    "module-resolver":
+      "https://cdn.jsdelivr.net/npm/babel-plugin-module-resolver@4.0.0/lib/index.min",
+  },
+});
+
+require(["babel!/main"]);
--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-require.config({
-  paths: {
-    babel: "babel",
+import Vue from "vue";
+import httpVueLoader from "http-vue-loader";
 
-    "babel-standalone":
-      "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min",
-    "babel-polyfill":
-      "https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min",
-    vue: "https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.11/vue.min",
-    "http-vue-loader":
-      "https://cdn.jsdelivr.net/npm/http-vue-loader@1.4.2/src/httpVueLoader.min",
-    axios: "https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min",
+Vue.use(httpVueLoader);
+
+new Vue({
+  components: {
+    app: "url:app.vue",
   },
-});
-
-require(["babel!app"]);
+  template: "<app />",
+}).$mount("#app");
