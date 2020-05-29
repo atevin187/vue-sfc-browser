ac2c5e16d60c0d0d722505f1051b216dc42b7e0c
Kamil Cholewa <atevin187@gmail.com>
2020-05-29 13:49:08 +0200

    Delete babel-polyfill from dependencies

--- core/main.js
+++ core/main.js
@@ -4,8 +4,6 @@ require.config({
 
     "babel-standalone":
       "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min",
-    "babel-polyfill":
-      "https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min",
     vue: "https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.11/vue.min",
     "http-vue-loader":
       "https://cdn.jsdelivr.net/npm/http-vue-loader@1.4.2/src/httpVueLoader.min",
